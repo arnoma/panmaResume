27de769c20865f6211ebd06784444c4f95e6dd6a
panma <arnoma2015@gmail.com>
2016-10-20 09:23:14 +0800

    init commit

---
+++ src/pages/home/home.controller.js
@@ -0,0 +1,12 @@
+"use strict";
+
+module.exports = ({_am,_ng} = __APP)=>{
+
+    _am.controller('HomePageController',function ($scope,$http) {
+
+        $scope.homePageString = 'Home Page';
+
+    });
+
+
+};
