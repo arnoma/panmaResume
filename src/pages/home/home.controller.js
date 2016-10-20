9192e3fb63b5af651660c8570702eaec97122054
panma <arnoma2015@gmail.com>
2016-10-20 11:59:12 +0800

    fix

--- src/pages/home/home.controller.js
+++ src/pages/home/home.controller.js
@@ -4,7 +4,7 @@ module.exports = ({_am,_ng} = __APP)=>{
 
     _am.controller('HomePageController',function ($scope,$http) {
 
-        $scope.homePageString = 'Home Page';
+        $scope.homePageString = 'Page';
 
     });
 
